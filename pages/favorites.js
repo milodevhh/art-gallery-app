f6944728c5a7759cb56783cf619ed19f10ab8b78
milodevhh <schmiesseklgmail.com>
2023-08-02 17:21:24 +0200

    add final function for fav page

--- pages/favorites.js
+++ pages/favorites.js
@@ -5,9 +5,21 @@ export default function FavoritePieces({
   handleToggle,
   artPiecesInfo,
 }) {
-  const onlyFavoritePieces = artPiecesInfo.filter(
-    (artPieceInfo) => artPieceInfo.isFavorite === true
+  const onlyFavoritePieces = pieces.filter((piece) =>
+    artPiecesInfo.find(
+      (artPiece) => artPiece.slug === piece.slug && artPiece.isFavorite
+    )
   );
+
+  ///*  1. Ziel = Wir wollen wissen welche pieces sind favorite
+  // Durchsuchen des Arrays (Datensatz) Key/Value Pärchen
+  // Dieses Array wollen wir filtern
+  // Im State haben wir die Info tur/False Heart SVG
+  // Welche Pieces im Pieces Array haben True/False?
+  // ArtPiecesInfo (State) mit .find suchen wir den piece
+  // Wenn true dann gib Objekt aus
+  // .filter wenn find ein undifined hat - das fällt weg */
+
   console.log(onlyFavoritePieces);
   return (
     <>
@@ -15,9 +27,9 @@ export default function FavoritePieces({
         "There are no favorites!"
       ) : (
         <ArtPieces
-          pieces={pieces}
+          pieces={onlyFavoritePieces}
           handleToggle={handleToggle}
-          artPiecesInfo={onlyFavoritePieces}
+          artPiecesInfo={artPiecesInfo}
         />
       )}
     </>
